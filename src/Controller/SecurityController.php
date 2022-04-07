3ff060a71b02a903c9cb63bc8944afe43ee9b2e9
Jerome Marras <jerome.marras@hotmail.fr>
2022-04-07 22:25:35 +0200

    Rajout CSS/homecontroller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,13 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
-{
+    {
+        //#[Route(path: '/login', name: 'app_login')]
+    //public function home(AuthenticationUtils $authenticationUtils): Response
+    //{
+        //return  new Response('vous etes deco');
+    //}
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser()) {
+             return $this->redirectToRoute('app_home');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
